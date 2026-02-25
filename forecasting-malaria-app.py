dae902ca544f4cc27ec15c3a8ac176358b699360
immanuelgoko07 <immanuelgoko07@gmail.com>
2026-02-25 13:46:15 +0300

    Refactor malaria forecasting app code structure

--- forecasting-malaria-app.py
+++ forecasting-malaria-app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 st.set_page_config(page_title="Malaria Forecasting System", layout="wide")
 
@@ -192,5 +191,6 @@ if uploaded_file:
 else:
 
     st.info("Please upload a CSV file to begin forecasting.")
+
 
 
