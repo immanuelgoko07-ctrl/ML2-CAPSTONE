ebc1faefb0e7ebe59ad92154b43c0d3da2bb6e5c
immanuelgoko07 <immanuelgoko07@gmail.com>
2026-03-06 16:12:38 +0300

    Update forecasting-malaria-app.py

--- forecasting-malaria-app.py
+++ forecasting-malaria-app.py
@@ -114,7 +114,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
 
 # -----------------------------
@@ -241,6 +240,7 @@ if uploaded_file is not None:
 
 else:
     st.info("Please upload a CSV file to get started.")
+
 
 
 
